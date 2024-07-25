13d43ee182b453a08238d3e32ab59b876f058a5a
codewithhamxa <hamxa.javed311@gmail.com>
2024-07-25 05:51:37 +0500

    done

---
+++ src/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Header.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+function Header() {
+  const [{basket}, dispatch] = useStateValue();
+  return (
+    <div className="header">
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <div className="header__logo">
+          <LocalMallIcon className="header__logoImage" fontSize="large" />
+          <h2 className="header__logoTitle">eStore</h2>
+        </div>
+      </Link>
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign In</span>
+        </div>
+
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+        <Link to="/checkout" style={{ textDecoration: "none" }}>
+          <div className="nav__itemBasket">
+            <ShoppingCartIcon />
+            <span className="nav__itemLineTwo nav__basketCount">{basket.length}</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
